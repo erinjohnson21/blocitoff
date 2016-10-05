def978651b357ca242010705110ed8636e318d06
erinjohnson21 <erinjohnson21@gmail.com>
2016-10-05 08:31:24 -0500

    Initial Commit

---
+++ src/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = React.createClass({
+  render() {
+    return (
+      <div className="List">Our list will go here.</div>
+    );
+  }
+});
+
+export default List;

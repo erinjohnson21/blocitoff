b4afd0a154aa832cdff457f137f850d8e26aa5ad
Erin Johnson <erinjohnson21@gmail.com>
2016-10-06 22:34:43 -0500

    Configure Firebase: Incomplete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import List from './List';
+import SignIn from './SignIn';
 
 class App extends Component {
   render() {
---
+++ src/SignIn.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+// firebase.auth().signInWithEmailAndPassword('erin@erinelizabethjohnson.com', 'monkeyisabadpassword').catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // ...
+//   console.log(error);
+// });
+//
+// firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     firebase.database().ref('todos').push({
+//       text: 'sample text',
+//       time: Date.now(),
+//       user: user.email,
+//     })
+// ;  } else {
+//     console.log('not signed in');
+//   }
+// });

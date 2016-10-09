4155e479cdfb1dbebc2b0b490a1f2a4efe0bcd0e
Erin Johnson <erinjohnson21@gmail.com>
2016-10-08 21:33:40 -0500

    Body of Todo List: incomplete

--- src/List.js
+++ src/List.js
@@ -3,11 +3,17 @@ import React from 'react';
 const List = React.createClass({
   render() {
     return (
-      <div className="List">
-        Our list will go here.
+      <div className="todoListMain-container">
+        <div className="header">
+          <h1>Things to Do</h1>
+          <form>
+            <input className="task" placeholder="Enter Task"></input><br/>
+            <button type="submit">Add Task</button>
+            <a className="link" href="#">Completed Tasks</a>
+            <a className="link" href="#">Expired Tasks</a>
+          </form>
 
-
-
+        </div>
       </div>
     );
   }

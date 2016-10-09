1a4408cf944838689310050fe479bb25a50a11d2
Erin Johnson <erinjohnson21@gmail.com>
2016-10-09 16:52:53 -0500

    View Active Tasks: Functional

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './List';
-import SignIn from './SignIn';
+// import SignIn from './SignIn';
 
 class App extends Component {
   render() {
@@ -15,7 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload ok.
         </p>
-        <List />
+        <List/>
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import TodoItems from './TodoItems';
 
-const List = React.createClass({
-  render() {
+var List = React.createClass({
+  getInitialState: function () {
+    return {
+      items: []
+    };
+  },
+
+  addItem: function (e) {
+    var itemArray = this.state.items;
+
+    itemArray.push({
+      text: this.inputElement.value,
+      key: Date.now()
+    }
+  );
+
+    this.setState({items: itemArray});
+
+    this.inputElement.value="";
+
+    e.preventDefault();
+  },
+
+  render: function () {
     return (
-      <div className="todoListMain-container">
+      <div className="todoListMain">
         <div className="header">
           <h1>Things to Do</h1>
-          <form>
-            <input className="task" placeholder="Enter Task"></input><br/>
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this.inputElement = a}
+              className="task" placeholder="Enter Task">
+            </input><br/>
             <button type="submit">Add Task</button>
-            <a className="link" href="#">Completed Tasks</a>
             <a className="link" href="#">Expired Tasks</a>
+            <a className="link" href="#">Completed Tasks</a>
           </form>
-
+        </div>
+        <div>
+          <TodoItems entries={this.state.items}/>
         </div>
       </div>
     );

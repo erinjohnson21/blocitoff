9bee5654716b4ec82e772242167741cda88ec7c8
Erin Johnson <erinjohnson21@gmail.com>
2016-10-11 20:31:04 -0500

    Configure Firebase: getting closer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 import List from './List';
-import SignIn from './SignIn';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCCqXrGQJfbECB3oM5HREOYKwL44NhFm-8",
+    authDomain: "todo-c4290.firebaseapp.com",
+    databaseURL: "https://todo-c4290.firebaseio.com",
+    storageBucket: "todo-c4290.appspot.com",
+    messagingSenderId: "213946233863"
+  };
+  firebase.initializeApp(config);
+
+  firebase.auth().signInWithEmailAndPassword('erin@erinelizabethjohnson.com', 'monkeyisabadpassword').catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log(error);
+  });
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      firebase.database().ref('todos').push({
+        // text: 'blah blah',
+        // time: Date.now(),
+        // user: user.email,
+      });
+    } else {
+      console.log('not signed in');
+    }
+  });
 
 
 
 class App extends Component {
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('todos');
+  }
+
   render() {
     return (
       <div className="App">
